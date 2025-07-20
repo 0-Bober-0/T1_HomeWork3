5fa9a83e020450eea54d7759c44418c0b540b842
Bober <s-daew@ya.ru>
2025-07-20 15:28:38 +0300

    initial project

---
+++ bishop-starter/src/main/java/ru/t1/daev/bishopstarter/BishopStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.daev.bishopstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BishopStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BishopStarterApplication.class, args);
+    }
+
+}
---
+++ bishop-starter/src/test/java/ru/t1/daev/bishopstarter/BishopStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.daev.bishopstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BishopStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
